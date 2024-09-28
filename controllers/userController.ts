101d15346922efe7cb0cd60de2857c07e226fd4a
eladjmc <eladjmc88@gmail.com>
2024-09-28 21:23:10 +0300

    changed the secret to env

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -6,7 +6,7 @@ import dotenv from "dotenv"
 
 dotenv.config();
 
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SECRET:string = process.env.JWT_SECRET || "default_secret";
 
 export const register = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
